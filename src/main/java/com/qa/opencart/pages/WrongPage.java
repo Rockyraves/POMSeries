95f1c091f7a564075f4671070f6cf6695842eac9
Rockyraves <raveesh28071996@gmail.com>
2024-05-07 19:55:22 +0530

    New Coomit

--- src/main/java/com/qa/opencart/pages/WrongPage.java
+++ src/main/java/com/qa/opencart/pages/WrongPage.java
@@ -3,7 +3,7 @@ package com.qa.opencart.pages;
 public class WrongPage {
 
 	public static void main(String[] args) {
-		System.out.println("New Project");
+		System.out.println("New Proj");
 	}
 
 }
